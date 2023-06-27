e7d5f96287fe9c3719e646831c04f003d76765c8
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2023-06-27 10:05:11 -0500

    LoopOfMap

---
+++ Assets/Scripts/LoopMap.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopMap : MonoBehaviour
+{
+    [SerializeField] GameObject Playa;
+    [SerializeField] GameObject Playa1;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Playa.transform.position = new Vector3(Playa.transform.position.x-speed*Time.deltaTime, Playa.transform.position.y, Playa.transform.position.z);
+        Playa1.transform.position = new Vector3(Playa1.transform.position.x - speed*Time.deltaTime, Playa1.transform.position.y, Playa1.transform.position.z);
+        if (Playa.transform.position.x <= -230)
+        {
+            Playa.transform.position = new Vector3(90.5f, Playa.transform.position.y, Playa.transform.position.z);
+        }
+        if (Playa1.transform.position.x <= -230)
+        {
+            Playa1.transform.position = new Vector3(90.5f, Playa1.transform.position.y, Playa1.transform.position.z);
+        }
+    }
+}
